dc0b07e8e326563860fc08f9e7b919d70200bc4d
Nesbitt <mnalband@lesley.edu>
2020-01-27 08:27:59 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Everything running smoothly!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
