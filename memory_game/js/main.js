90505dc2928521a9420e2f93cea6bf0a8f2052df
Nesbitt <mnalband@lesley.edu>
2020-02-13 09:07:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,21 +8,29 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 */
 const cards = ["queen", "queen", "king", "king"]
-const cardsInPlay = []	
-	let cardOne = cards[0];
-	let cardTwo = cards[1];
-	let cardThree = cards[2];
-	let cardFour = cards[3];
+const cardsInPlay = []
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]){
-	alert("Sorry, try again"); 
- }
-}	
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+};
+
+function flipCard(cardId) {	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
