5f5c2116bb21a8cf61c05952e2d264cc0d2e6a62
Nesbitt <mnalband@lesley.edu>
2020-03-01 21:37:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,3 @@
-/*console.log("Everything running smoothly!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-*/
 const cards = [ 
 	{
 		rank: "queen",
@@ -28,33 +19,73 @@ const cards = [
 		suit: "diamonds",
 		cardImage: "images/king-of-diamonds.png"
 	}
-
 ];
 
-const cardsInPlay = []
+let cardsInPlay = [];
+let playerScore = 0;
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		const cardElement = document.createElement('img'); 
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('reset').addEventListener('click', resetBoard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+function flipCard() {
+	/*use getAttribute to identify the index of the card that has been clicked and store it in variable
+	*/
+	const cardIndex = this.getAttribute('data-id');
+	/*we use the clicked card index to get the keys and values from the clicked card
+	*/
+	const keysAndValues = cards[cardIndex];
+	/*Now we have the information from each card based on its index. What we want is the card image 
+	*/
+	const imagePath = keysAndValues['cardImage'];
+	const cardRank = keysAndValues['rank'];
+	/*Now we have the card image. We have to set the card's source attribute to reflect the image path
+	*/
+	this.setAttribute('src', imagePath);
+	/*print the rank of the card to the console
+	*/
+	console.log("User flipped " + cardRank);
+	cardsInPlay.push(cardRank);
+	checkForMatch();
+}
 
 function checkForMatch() {
+	/*First we must make sure that two cards have been clicked.
+	Then, we need to determine whether card 1 and card 2 have the same rank.
+	If they do, the user should receive a pop up telling them they're found a match.
+	Otherwise, they should receive a pop up letting them know they have not made a match.*/
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-	  		alert("You found a match!");
+			/*now, we want to record player's score. Player scores 1 point for a match, and 
+			1 point is subtracted for no match*/		
+			playerScore++; 
+			alert(`You got a match! User's score is ${playerScore} .`);
 		} else {
-	  		alert("Sorry, try again.");
+			playerScore--;
+			alert(`Sorry, try again. User's score is ${playerScore} .`);
 		}
 	}
-};
+}			
 
-function flipCard(cardId) {	
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+function resetBoard() {
+	/*in order to reset the board, we need to add a click event that will empty the array cardsInPlay.
+	We also need to clear and reset the game board*/
+	const board = document.getElementById('game-board');
+	board.innerHTML = "";
+	cardsInPlay = [];
+	createBoard();
+}
 
-	checkForMatch();
+createBoard();
 
-};
 
-flipCard(0);
-flipCard(2);
 
 
 
